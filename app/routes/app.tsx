c63241115f900f582c6482677648907cd77d5a9d
Oleksii Lavka <olavka@splitdev.com>
2025-10-21 11:42:19 +0300

    update error boundary

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -35,7 +35,7 @@ export default function App() {
 export function ErrorBoundary() {
   const error = useRouteError();
   console.error('============');
-  console.error(error?.message);
+  console.error(error);
   console.error('============');
 
   return <div>error</div>;
