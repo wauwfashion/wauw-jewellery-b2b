ac7335eef3fc8a590cf9e30407b932bfac507d81
Oleksii Lavka <olavka@splitdev.com>
2025-10-22 14:53:25 +0300

    added general webhook error logs

--- app/routes/webhooks.tsx
+++ app/routes/webhooks.tsx
@@ -5,44 +5,48 @@ import { webhookHandlers } from '@/api/webhooks/shopify';
 import { WebhookTopic } from '@/types';
 
 export const action = async ({ request, context }: ActionFunctionArgs) => {
-  const shop = request.headers.get('x-shopify-shop-domain') as string;
-  const topic = (request.headers.get('x-shopify-topic') as string)
-    .replace('/', '_')
-    .toUpperCase() as WebhookTopic;
+  try {
+    const shop = request.headers.get('x-shopify-shop-domain') as string;
+    const topic = (request.headers.get('x-shopify-topic') as string)
+      .replace('/', '_')
+      .toUpperCase() as WebhookTopic;
 
-  console.log(`Received webhook - topic: ${topic}, shop: ${shop}`)
+    console.log(`Received webhook - topic: ${topic}, shop: ${shop}`)
 
-  const { admin } = await unauthenticated.admin(shop);
+    const { admin } = await unauthenticated.admin(shop);
 
-  if (!admin) {
-    throw new Response('Admin is not exist');
+    if (!admin) {
+      throw new Response('Admin is not exist');
+    }
+
+    if (!WebhookTopic[topic]) {
+      return new Response('Unhandled webhook topic', { status: 404 });
+    }
+
+    if (
+      [
+        WebhookTopic.CUSTOMERS_REDACT,
+        WebhookTopic.SHOP_REDACT,
+        WebhookTopic.CUSTOMERS_DATA_REQUEST,
+      ].includes(topic as WebhookTopic)
+    ) {
+      return new Response('OK', { status: 200 });
+    }
+
+    const webhookHandler = webhookHandlers[topic as WebhookTopic];
+
+    if (!webhookHandler) {
+      throw new Response('Unhandled webhook topic', { status: 404 });
+    }
+
+    const response = await webhookHandler({
+      request,
+      shop,
+      graphql: admin.graphql,
+    });
+
+    return response || null;
+  } catch (err) {
+    console.log('Webhook received error: ', err)
   }
-
-  if (!WebhookTopic[topic]) {
-    return new Response('Unhandled webhook topic', { status: 404 });
-  }
-
-  if (
-    [
-      WebhookTopic.CUSTOMERS_REDACT,
-      WebhookTopic.SHOP_REDACT,
-      WebhookTopic.CUSTOMERS_DATA_REQUEST,
-    ].includes(topic as WebhookTopic)
-  ) {
-    return new Response('OK', { status: 200 });
-  }
-
-  const webhookHandler = webhookHandlers[topic as WebhookTopic];
-
-  if (!webhookHandler) {
-    throw new Response('Unhandled webhook topic', { status: 404 });
-  }
-
-  const response = await webhookHandler({
-    request,
-    shop,
-    graphql: admin.graphql,
-  });
-
-  return response || null;
 };
